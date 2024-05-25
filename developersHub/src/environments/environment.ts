56293e7ac001d732d2910cfc327c7f956df52682
Anand <108626421+soup-stix@users.noreply.github.com>
2024-05-25 11:56:49 +0530

    Update environment.ts

--- developersHub/src/environments/environment.ts
+++ developersHub/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000/',
     pvtUrl: 'http://localhost:3000/pvt/',
-    cacheKey: 'yuidfuioghzf89y78247123r7834r68sdfsdeo',
-    token: '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'
+    cacheKey: 'yuidxxxxxxxxxxxxxxxxxxxx',
+    token: 'ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   };
